e1ef2d411bca8d2a4d637ef7e574e3f5620bb576
Herby <herbyosbourne31@gmail.com>
2025-07-09 13:37:44 -0500

    update main screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,19 @@
 import { StyleSheet } from "react-native-unistyles";
-import { Text, View } from "react-native";
+import { ScrollView } from "react-native";
+import { ThemeText } from "@/components/ThemeText";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={{}}> Home Screen</Text>
-      <View style={{}} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <ThemeText type="title">Playlist</ThemeText>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create((theme) => ({
+const styles = StyleSheet.create((theme, rt) => ({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: rt.insets.top,
+    paddingHorizontal: theme.gap(2),
   },
 }));
