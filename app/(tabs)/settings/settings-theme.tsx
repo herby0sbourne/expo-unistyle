4f1e5ff796a149a1e50a5a4ac6093555ddf9db1b
Herby <herbyosbourne31@gmail.com>
2025-07-10 13:26:19 -0500

    added functionality to switch between light and dark theme

--- app/(tabs)/settings/settings-theme.tsx
+++ app/(tabs)/settings/settings-theme.tsx
@@ -1,17 +1,37 @@
 import { ScrollView, View } from "react-native";
-import { StyleSheet } from "react-native-unistyles";
+import { StyleSheet, UnistylesRuntime, useUnistyles } from "react-native-unistyles";
 import SettingOptionRadio from "@/components/SettingOptionRadio";
 import ThemeColor from "@/components/ThemeColor";
 
 const SettingThemeScreen = () => {
+  const { rt } = useUnistyles();
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <SettingOptionRadio label="System" isSelected={false} onPess={() => {}} />
-      <SettingOptionRadio label="User" isSelected={false} onPess={() => {}} />
-      <View style={styles.row}>
-        <ThemeColor label="light" onPress={() => {}} />
-        <ThemeColor label="dark" onPress={() => {}} />
-      </View>
+      <SettingOptionRadio
+        label="System"
+        isSelected={rt.hasAdaptiveThemes}
+        onPess={() => {
+          if (rt.hasAdaptiveThemes) {
+            return;
+          }
+          UnistylesRuntime.setAdaptiveThemes(true);
+        }}
+      />
+      <SettingOptionRadio
+        label="User"
+        isSelected={!rt.hasAdaptiveThemes}
+        onPess={() => {
+          if (rt.hasAdaptiveThemes) {
+            UnistylesRuntime.setAdaptiveThemes(false);
+          }
+        }}
+      />
+      {!rt.hasAdaptiveThemes && (
+        <View style={styles.row}>
+          <ThemeColor label="light" onPress={() => UnistylesRuntime.setTheme("light")} />
+          <ThemeColor label="dark" onPress={() => UnistylesRuntime.setTheme("dark")} />
+        </View>
+      )}
     </ScrollView>
   );
 };
