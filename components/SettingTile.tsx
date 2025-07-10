06bdf4563645d6dea20b59f83626bac92dede099
Herby <herbyosbourne31@gmail.com>
2025-07-10 12:18:17 -0500

    create componet for reuable tile/button

---
+++ components/SettingTile.tsx
@@ -0,0 +1,38 @@
+import { FC } from "react";
+import { View, Pressable, PressableStateCallbackType } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
+import { ThemeText } from "./ThemeText";
+
+type SettingTileProps = {
+  settingName: string;
+  settingValue: string;
+  description: string;
+  onPress(): void;
+  // press: () => void;
+};
+
+export const SettingTile: FC<SettingTileProps> = ({
+  settingName,
+  settingValue,
+  description,
+  onPress,
+}) => {
+  return (
+    <Pressable onPress={onPress} style={styles.container}>
+      <View>
+        <ThemeText bold>{settingName}</ThemeText>
+        <ThemeText dimmed>{description}</ThemeText>
+        <ThemeText>{settingValue}</ThemeText>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: (state: PressableStateCallbackType) => ({
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    opacity: state.pressed ? 0.75 : 1,
+  }),
+});
