eda6d76db9f56b6a01e856bec3ba2a7dc095a143
Herby <herbyosbourne31@gmail.com>
2025-07-11 13:37:30 -0500

    added type to TextProps

--- components/ThemeText.tsx
+++ components/ThemeText.tsx
@@ -1,4 +1,4 @@
-import { Text, TextProps } from "react-native";
+import { Text, type TextProps } from "react-native";
 import React from "react";
 import { StyleSheet, type UnistylesVariants } from "react-native-unistyles";
 
