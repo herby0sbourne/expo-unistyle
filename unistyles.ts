bcff157b99ee5f0b55226af51c47728e4efd9dd1
Herby <herbyosbourne31@gmail.com>
2025-07-16 22:45:06 -0500

    export accent options

--- unistyles.ts
+++ unistyles.ts
@@ -1,49 +1,49 @@
-import { StyleSheet } from 'react-native-unistyles'
+import { StyleSheet } from "react-native-unistyles";
 
 const lightTheme = {
   colors: {
-    background: '#FCFAF8',
-    foreground: '#EDEAE6',
-    typography: '#1B140C',
-    dimmed: '#ECE8E4',
-    tint: '#9A734C',
-    activeTint: '#1B140C',
-    link: '#1E3799',
+    background: "#FCFAF8",
+    foreground: "#EDEAE6",
+    typography: "#1B140C",
+    dimmed: "#ECE8E4",
+    tint: "#9A734C",
+    activeTint: "#1B140C",
+    link: "#1E3799",
     accents: {
-      banana: '#F6E58D',
-      pumpkin: '#FFBE76',
-      apple: '#FF7979',
-      grass: '#BADC58',
-      storm: '#686DE0'
-    }
+      banana: "#F6E58D",
+      pumpkin: "#FFBE76",
+      apple: "#FF7979",
+      grass: "#BADC58",
+      storm: "#686DE0",
+    },
   },
   gap: (v: number) => v * 8,
-} as const
+} as const;
 
 const darkTheme = {
   colors: {
-    background: '#221A11',
-    foreground: '#332618',
-    typography: '#FFFFFF',
-    dimmed: '#A8A198',
-    tint: '#C9AD92',
-    activeTint: '#FFFFFF',
-    link: '#0C2461',
+    background: "#221A11",
+    foreground: "#332618",
+    typography: "#FFFFFF",
+    dimmed: "#A8A198",
+    tint: "#C9AD92",
+    activeTint: "#FFFFFF",
+    link: "#0C2461",
     accents: {
-      banana: '#f9CA24',
-      pumpkin: '#F0932B',
-      apple: '#EB4D4B',
-      grass: '#6AB04C',
-      storm: '#4834D4'
-    }
+      banana: "#f9CA24",
+      pumpkin: "#F0932B",
+      apple: "#EB4D4B",
+      grass: "#6AB04C",
+      storm: "#4834D4",
+    },
   },
   gap: (v: number) => v * 8,
-} as const
+} as const;
 
 const appThemes = {
   light: lightTheme,
-  dark: darkTheme
-}
+  dark: darkTheme,
+};
 
 const breakpoints = {
   xs: 0,
@@ -51,23 +51,26 @@ const breakpoints = {
   md: 500,
   lg: 800,
   xl: 1200,
-}
+};
 
-type AppBreakpoints = typeof breakpoints
-type AppThemes = typeof appThemes
+export type Accents = keyof (typeof lightTheme)["colors"]["accents"];
 
-declare module 'react-native-unistyles' {
+type AppBreakpoints = typeof breakpoints;
+type AppThemes = typeof appThemes;
+
+declare module "react-native-unistyles" {
   export interface UnistylesThemes extends AppThemes {}
+
   export interface UnistylesBreakpoints extends AppBreakpoints {}
 }
 
 StyleSheet.configure({
   settings: {
-    adaptiveThemes: true
+    adaptiveThemes: true,
   },
   themes: {
     light: lightTheme,
     dark: darkTheme,
   },
   breakpoints,
-})
+});
