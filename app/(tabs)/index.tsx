bfbcef57289c5bb9be1bc20b8cb30cc7d8fe891a
Herby <herbyosbourne31@gmail.com>
2025-07-16 21:11:56 -0500

    update Home screen to show list of songTile

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,47 @@
+import { ScrollView, View } from "react-native";
+import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native-unistyles";
-import { ScrollView } from "react-native";
+
+import SongTile from "@/components/SongTile";
 import { ThemeText } from "@/components/ThemeText";
 
-export default function TabOneScreen() {
+import { playlist } from "@/mocks";
+
+export default function PlaylistScreen() {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <ThemeText type="title">Playlist</ThemeText>
-    </ScrollView>
+    <>
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+          <View style={styles.header}>
+            <ThemeText type="title">Playlist</ThemeText>
+          </View>
+
+          {playlist.map((song) => (
+            <SongTile
+              key={song.id}
+              song={song}
+              onPress={() => router.push(`/(tabs)/player/${song.id}`)}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create((theme, rt) => ({
   container: {
-    flex: 1,
-    marginTop: rt.insets.top,
+    marginTop: rt.insets.top + theme.gap(3),
+    backgroundColor: theme.colors.background,
+  },
+  contentContainer: {
+    gap: theme.gap(3),
     paddingHorizontal: theme.gap(2),
+    paddingBottom: rt.insets.bottom,
+  },
+  header: {
+    paddingBottom: theme.gap(2),
   },
 }));
+// <StatusBar backgroundColor="green" />
